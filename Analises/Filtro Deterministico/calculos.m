cf867c08119f2a8146ed941db38d7e89742059c1
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2021-02-04 17:01:48 -0300

    update graficos

--- Analises/Filtro Deterministico/calculos.m
+++ Analises/Filtro Deterministico/calculos.m
@@ -2,92 +2,92 @@ load('dados\Lado-Canal.mat');
 load('workspace\energia_MeV.mat');
 
 %% Criando matrizes separando por lado, canal e modulo
-L0C0M0 = [];
-L0C1M0 = [];
-L0C2M0 = [];
-L0C3M0 = [];
+L0C0M13 = [];
+L0C1M13 = [];
+L0C2M13 = [];
+L0C3M13 = [];
 
-m=1;
+m=13;
 
 for i=m:64:3224640
-   L0C0M0 = [L0C0M0; L0C0(i,4:10)];
+   L0C0M13 = [L0C0M13; L0C0(i,4:10)];
 end
 
 for i=m:64:3224640
-   L0C1M0 = [L0C1M0; L0C1(i,4:10)];
+   L0C1M13 = [L0C1M13; L0C1(i,4:10)];
 end
 
 for i=m:64:3224640
-   L0C2M0 = [L0C2M0; L0C2(i,4:10)];
+   L0C2M13 = [L0C2M13; L0C2(i,4:10)];
 end
 
 for i=m:64:3224640
-   L0C3M0 = [L0C3M0; L0C3(i,4:10)];
+   L0C3M13 = [L0C3M13; L0C3(i,4:10)];
 end
 
 %% Filtrando os dados pra um valor de corte em MeV
 load('workspace\energia_MeV')
 
-FL0C0M0 = [];
-FL0C1M0 = [];
-FL0C2M0 = [];
-FL0C3M0 = [];
+FL0C0M13 = [];
+FL0C1M13 = [];
+FL0C2M13 = [];
+FL0C3M13 = [];
 
 for i=1:50385
-    if Ma1(i,1)>150
-        FL0C0M0 = [FL0C0M0; L0C0M0(i,:)];
+    if Ma12(i,1)>150
+        FL0C0M13 = [FL0C0M13; L0C0M13(i,:)];
     end
 end
 
 for i=1:50385
-    if Ma1(i,2)>150
-        FL0C1M0 = [FL0C1M0; L0C1M0(i,:)];
+    if Ma12(i,2)>150
+        FL0C1M13 = [FL0C1M13; L0C1M13(i,:)];
     end
 end
 
 for i=1:50385
-    if Ma1(i,3)>150
-        FL0C2M0 = [FL0C2M0; L0C2M0(i,:)];
+    if Ma12(i,3)>150
+        FL0C2M13 = [FL0C2M13; L0C2M13(i,:)];
     end
 end
 
 for i=1:50385
-    if Ma1(i,4)>150
-        FL0C3M0 = [FL0C3M0; L0C3M0(i,:)];
+    if Ma12(i,4)>150
+        FL0C3M13 = [FL0C3M13; L0C3M13(i,:)];
     end
 end
 
 %% Normalizando os dados
 
-NFL0C0M0 = FL0C0M0(:,:);
-for i=1:23344
-    div = norm(NFL0C0M0(i,:));
+NFL0C0M13 = FL0C0M13(:,:);
+for i=1:11901
+    div = norm(NFL0C0M13(i,:));
 for j=1:7
-    NFL0C0M0(i,j)=NFL0C0M0(i,j)/div;
+    NFL0C0M13(i,j)=NFL0C0M13(i,j)/div;
 end
 end
 
-NFL0C1M0 = FL0C1M0(:,:);
-for i=1:23634
-    div = norm(NFL0C1M0(i,:));
+NFL0C1M13 = FL0C1M13(:,:);
+for i=1:12203
+    div = norm(NFL0C1M13(i,:));
 for j=1:7
-    NFL0C1M0(i,j)=NFL0C1M0(i,j)/div;
+    NFL0C1M13(i,j)=NFL0C1M13(i,j)/div;
 end
 end
 
-NFL0C2M0 = FL0C2M0(:,:);
-for i=1:42738
-    div = norm(NFL0C2M0(i,:));
+NFL0C2M13 = FL0C2M13(:,:);
+for i=1:49606
+    div = norm(NFL0C2M13(i,:));
 for j=1:7
-    NFL0C2M0(i,j)=NFL0C2M0(i,j)/div;
+    NFL0C2M13(i,j)=NFL0C2M13(i,j)/div;
 end
 end
 
-NFL0C3M0 = FL0C3M0(:,:);
-for i=1:43371
-    div = norm(NFL0C3M0(i,:));
+NFL0C3M13 = FL0C3M13(:,:);
+for i=1:49777
+    div = norm(NFL0C3M13(i,:));
 for j=1:7
-    NFL0C3M0(i,j)=NFL0C3M0(i,j)/div;
+    NFL0C3M13(i,j)=NFL0C3M13(i,j)/div;
 end
 end
 
